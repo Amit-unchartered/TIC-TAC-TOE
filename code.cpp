541c98fd0db2ce68f1362c945b728b24011694cc
Amit-unchartered <amitkumaragrawal42@gmail.com>
2024-04-05 06:02:23 +0530

    checking columns

--- code.cpp
+++ code.cpp
@@ -16,6 +16,23 @@ int checkwin(){
     if(square[7] == square[8] && square[8] == square[9]){
         return 1;
     }
+
+    //checking columns
+    if(square[1] == square[4] && square[4] == square[7]){
+        return 1;
+    }
+    else if(square[2] == square[5] && square[5] == square[8]){
+        return 1;
+    }
+    else if(square[3] == square[6] && square[6] == square[9]){
+        return 1;
+    }
+    else if(square[1] == square[5] && square[5] == square[9]){
+        return 1;
+    }
+    else if(square[3] == square[5] && square[5] == square[7]){
+        return 1;
+    }
 }
 
 int main(){
